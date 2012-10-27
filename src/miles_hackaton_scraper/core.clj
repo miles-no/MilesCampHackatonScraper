791a66f7ba63891189ed1ffa4247e58210af5721
Erik Bakstad <erik.bakstad@gmail.com>
2012-10-27 18:14:59 +0200

    Cleanup

--- src/miles_hackaton_scraper/core.clj
+++ src/miles_hackaton_scraper/core.clj
@@ -1,33 +1,21 @@
 (ns miles-hackaton-scraper.core
   (:require [net.cgrand.enlive-html :as html]
             [cheshire.core :as json]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [miles-hackaton-scraper.tokenizer :as t]
+            [clojure.string :as str]))
 
-(def *base-url* "http://miles.no")
-
-(def *bergen-people* "http://www.miles.no/menneskene/bergen")
-(def *oslo-people* "http://www.miles.no/menneskene/oslo")
-(def *stavanger-people* "http://www.miles.no/menneskene/stavanger")
-
-(defn get-hash [type data]
-  (.digest (java.security.MessageDigest/getInstance type) (.getBytes data) ))
-
-(defn sha1-hash [data]
-  (String/format "%1$02x" (get-hash "sha" data)))
-
-(defn hexify [s]
-  (apply str
-    (map #(format "%02x" (int %)) s)))
+(def bergen-emp "http://www.miles.no/menneskene/bergen")
+(def oslo-emp "http://www.miles.no/menneskene/oslo")
+(def stavanger-emp "http://www.miles.no/menneskene/stavanger")
 
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
 
-(defn people [city]
+(defn links-to-people [city]
   (let [links (html/select (fetch-url city) [:#ansattListe :li (html/nth-child 2) ])]
-    (map second (reduce (fn [so-far i]
-                          (let [name (first (:content i)) 
-                                url (get-in i [:attrs :href])]
-                            (assoc so-far name url))) {} links))))
+    (reduce (fn [so-far i]
+              (conj so-far (get-in i [:attrs :href]))) [] links)))
 
 (defn extract-role [text]
   (let [string (second (re-find #"Rolle:\s(.+)$" text))]
@@ -38,28 +26,33 @@
 
 (defn get-info [link city]
   (let [page (fetch-url link)
-        details (html/select (fetch-url link) [:#medarbeiderVisning :p])
         image-link (get-in (first (html/select page [:.ansattbildeContainer :img])) [:attrs :src])
         id  (hash image-link)
-        full-name (first (html/select page [:#medarbeiderVisning :h2 html/content]))
-        role (extract-role (first (:content (first details))))
-        bio (extract-bio (first (:content (second details))))]
+        full-name (first (html/select page [:#medarbeiderVisning :> :h2 html/content]))
+        role (extract-role (str/join (html/select page [:#medarbeiderVisning :> :p html/html-content])))
+        bio (extract-bio (str/join (html/select page [:#medarbeiderVisning :.textController :p :> html/content])))]
     {:id id :name full-name :image image-link :role role :bio bio :branch city}))
 
 (defn get-branch [branch city]
-  (reduce (fn [so-far i] (conj so-far (get-info i city))) [] (people branch)))
+  (reduce
+   (fn [so-far i] (conj so-far (get-info i city)))
+   [] (links-to-people branch)))
+
+
+(defn scrape-emps []
+  (let [oslo (get-branch oslo-emp "Oslo")
+        bergen (get-branch bergen-emp "Bergen")
+        stavanger (get-branch stavanger-emp "Stavanger")]
+    (concat oslo bergen stavanger)))
 
 
 (defn extract-all []
-  (let [oslo (get-branch *oslo-people* "Oslo")
-        bergen (get-branch *bergen-people* "Bergen")
-        stavanger (get-branch *stavanger-people* "Stavanger")]
-    (flatten (conj [] oslo bergen stavanger))))
-
-(def all
-  (extract-all))
+  (let [emps (scrape-emps)]
+    (t/add-tags emps)))
 
 (defn print-file [name data]
-    (with-open [wrtr (io/writer name)]
+  (with-open [wrtr (io/writer name)]
     (.write wrtr data)))
 
+(defn dump-json [filename]
+  (print-file filename (json/generate-string (extract-all))))
--- src/miles_hackaton_scraper/tokenizer.clj
+++ src/miles_hackaton_scraper/tokenizer.clj
@@ -1,28 +1,26 @@
 (ns miles-hackaton-scraper.tokenizer
   (:require [clojure.set :as set]
-            [clojure.string :as str])
-  (:use miles-hackaton-scraper.core))
+            [clojure.string :as str]))
 
 (defn stopwords []
   (set (seq (.split (slurp "resources/stopwords.txt") "\n"))))
 
-
 (defn not-blank? [str]
   (not (str/blank? str)))
 
-(defn short? [str]
+(defn not-short? [str]
   (< 3 (.length str)))
 
 (defn split-bio [bio]
-  (filter short? (set/difference (set (str/split bio #"[\s;:\.\-\\,()/]")) (stopwords))))
+  (filter not-short? (set/difference (set (str/split bio #"[\s;:\.\-\\,()/]")) (stopwords))))
 
-(defn all-bios []
-  (reduce (fn [so-far i] (assoc so-far (:id i) (split-bio (.toLowerCase (:bio i))) )) {} all))
+(defn all-bios [all-emps]
+  (reduce (fn [so-far i] (assoc so-far (:id i) (split-bio (.toLowerCase (:bio i))) )) {} all-emps))
 
-(defn add-tags []
-  (let [bios (all-bios)]
+(defn add-tags [all-emps]
+  (let [bios (all-bios all-emps)]
     (map
      (fn [person] (let [id (:id person)
                        bio (get bios id)]
                    (assoc person :tags bio)))
-     all)))
+     all-emps)))
